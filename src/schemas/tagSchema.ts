55a3577e52c0dca2c175b37509969618b1cc3938
Vincent Coccina <v.coccina@acs2i.fr>
2024-07-18 11:02:58 +0200

    tag schema update

--- src/schemas/tagSchema.ts
+++ src/schemas/tagSchema.ts
@@ -2,8 +2,10 @@ import mongoose, { Document, ObjectId } from "mongoose";
 
 
 export interface Tag extends Document {
+    code: string,
     name: string
     level: string
+    tagGrouping_id: ObjectId
     status: string
     additional_fields?: any
     creator_id: ObjectId
@@ -11,9 +13,13 @@ export interface Tag extends Document {
 }
 
 const tagSchema = new mongoose.Schema<Tag>({
+    code: { type: String },
     name: { type: String },
     status: { type: String },
     level: { type: String },
+    tagGrouping_id: {
+        type: mongoose.Types.ObjectId
+    },
     additional_fields:{
         type: Map,
         of: mongoose.Schema.Types.Mixed
