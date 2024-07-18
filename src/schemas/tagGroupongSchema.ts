a933e145e7c8bea1ced0a62a6506a75f45307ff1
Vincent Coccina <v.coccina@acs2i.fr>
2024-07-18 10:46:23 +0200

    fix

--- src/schemas/tagGroupongSchema.ts
+++ src/schemas/tagGroupongSchema.ts
@@ -1,26 +1,26 @@
 import mongoose, { Document, ObjectId } from "mongoose";
 
 
-export interface Tag extends Document {
+export interface TagGrouping extends Document {
     name: string
-    level: string
+    level: string[]
     status: string
     additional_fields?: any
     creator_id: ObjectId
 
 }
 
-const tagSchema = new mongoose.Schema<Tag>({
+const tagGroupingSchema = new mongoose.Schema<TagGrouping>({
     name: { type: String },
+    level: [{ type: String }],
     status: { type: String },
-    level: { type: String },
     additional_fields:{
         type: Map,
         of: mongoose.Schema.Types.Mixed
     },
     creator_id: { type: mongoose.Types.ObjectId }
 
-}, { timestamps: true, collection: "tag"  })
+}, { timestamps: true, collection: "tag_grouping"  })
 
-const TagModel = mongoose.model<Tag>("tag", tagSchema)
-export default TagModel
+const TagGroupingModel = mongoose.model<TagGrouping>("tag_grouping", tagGroupingSchema)
+export default TagGroupingModel
