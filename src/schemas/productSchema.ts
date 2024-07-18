14f4dd336532c4dca8741f1ad21d7790d84ba032
Vincent Coccina <v.coccina@acs2i.fr>
2024-07-18 09:18:22 +0200

    product schema update

--- src/schemas/productSchema.ts
+++ src/schemas/productSchema.ts
@@ -1,35 +1,38 @@
 import mongoose ,{ Document, Model, ObjectId} from "mongoose"
 
 export interface Product extends Document {
+    creator_id: ObjectId 
+    reference: string,
     name: string,
     short_label: string,
     long_label: string,
     type: string,
     tag_ids: ObjectId[],
-    supplier_id: ObjectId, // supplier_id c'est le fournisseur principal 
+    princ_supplier_id: ObjectId, // supplier_id c'est le fournisseur principal 
     supplier_ids: ObjectId[] // supplier_ids c'est tout les fournisseurs qui vendent le produit
     dimension_types: string[]
     uvc_ids: ObjectId[],
     brand_ids: ObjectId[],
     collection_ids: ObjectId[], // collection is product collection
     status: string
-    creator_id: ObjectId 
     additional_fields: any
 }
 
 
 const productSchema = new mongoose.Schema<Product>({
     creator_id: {type: mongoose.Types.ObjectId},
+    reference: {type: String},
     name: {type: String},
-    long_label: {type: String},
     short_label: {type: String},
-    supplier_id: {type: mongoose.Types.ObjectId, ref: "supplier"},
+    long_label: {type: String},
+    type: {type: String},
     tag_ids: [{type: mongoose.Types.ObjectId, ref: "tag"}],
-    brand_ids: [{type: mongoose.Types.ObjectId, ref: "brand"}],
-    collection_ids: [{type: mongoose.Types.ObjectId, ref: "collection"}],
+    princ_supplier_id: {type: mongoose.Types.ObjectId, ref: "supplier"},
+    supplier_ids: [{type: mongoose.Types.ObjectId, ref: "supplier"}],
     dimension_types: [{type: String}],
     uvc_ids: [{type: mongoose.Types.ObjectId, ref: "uvc"}],
-    type: {type: String},
+    brand_ids: [{type: mongoose.Types.ObjectId, ref: "brand"}],
+    collection_ids: [{type: mongoose.Types.ObjectId, ref: "collection"}],
     status: {type: String},
     additional_fields:{
         type: Map,
